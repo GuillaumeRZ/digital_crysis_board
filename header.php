32ef6ac3bed9e1c0872f46f964714834fe52bbed
Gouigoui <DarkGuiguii@gmail.com>
2016-10-23 13:05:45 +0200

    Update with new archict.

--- header.php
+++ header.php
@@ -1,28 +1,56 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Digital Crysis Board</title>
-  <link rel="stylesheet" href="assets/css/menu.css">
-  <script src="assets/libraries/jquery.js"></script>
-  <script src="assets/js/deleteTeam.js"></script>
-</head>
-<body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Digital Crisis Board</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/menu.css">
+    <link rel="stylesheet" href="assets/css/progress.css">
+    <link rel="stylesheet" href="assets/css/background.css">
+    <link rel="stylesheet" href="assets/css/fonteffect.css">
+  </head>
 
-<nav class="menu">
-    <input type="checkbox" href="#" class="menu-open" name="menu-open" id="menu-open" />
-    <label class="menu-open-button" for="menu-open">
-      <span class="lines line-1"></span>
-      <span class="lines line-2"></span>
-      <span class="lines line-3"></span>
-    </label>
+  <body>
+    <div id="back"></div>
+    <div id="front"></div>
 
-    <a href="index.php" class="menu-item blue">Index</a>
-    <a href="config.php" class="menu-item green">Config</a>
-    <a href="addTeam.php" class="menu-item red"> AddTeam </a>
-    <a href="displayTeam.php" class="menu-item purple"> Equipes </a>
-    <!-- <a href="#" class="menu-item orange"> <i class="fa fa-star"></i> </a>
-    <a href="#" class="menu-item lightblue"> <i class="fa fa-diamond"></i> </a> -->
-</nav>
+    <svg viewBox="0 0 960 300">
+    	<symbol id="s-text">
+    		<text text-anchor="middle" x="50%" y="80%">Digital-Crisis</text>
+    	</symbol>
+
+    	<g class = "g-ants">
+    		<use xlink:href="#s-text" class="text-copy"></use>
+    		<use xlink:href="#s-text" class="text-copy"></use>
+    		<use xlink:href="#s-text" class="text-copy"></use>
+    		<use xlink:href="#s-text" class="text-copy"></use>
+    		<use xlink:href="#s-text" class="text-copy"></use>
+    	</g>
+    </svg>
+
+    <nav class="menu">
+      <input type="checkbox" class="nav__cb" id="menu-cb"/>
+      <div class="nav__content">
+        <ul class="nav__items">
+          <li class="nav__item">
+            <span class="nav__item-text">
+              <a href="index.php">Accueil</a>
+            </span>
+          </li>
+          </li>
+          <li class="nav__item">
+            <span class="nav__item-text">
+              <a href="https://tweetdeck.twitter.com/" target="_blank">TweetDeck</a>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <label class="nav__btn" for="menu-cb"></label>
+    </nav>
--- index.php
+++ index.php
@@ -1,3 +1,50 @@
 <?php include 'header.php'; ?>
 
-<h1>Index - Digital Crysis</h1>
+<div class="progress-container">
+  <div class="bar-main-container azure">
+    <div class="wrap">
+      <div class="bar-percentage" data-percentage="60">Alpha - 50 Pts</div>
+      <div class="bar-container">
+          <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bar-main-container emerald">
+    <div class="wrap">
+      <div class="bar-percentage" data-percentage="60">Bravo - 50 Pts</div>
+      <div class="bar-container">
+          <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bar-main-container violet">
+    <div class="wrap">
+      <div class="bar-percentage" data-percentage="60">Charlie - 50 Pts</div>
+      <div class="bar-container">
+          <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bar-main-container yellow">
+    <div class="wrap">
+      <div class="bar-percentage" data-percentage="60">Tango - 50 Pts</div>
+      <div class="bar-container">
+          <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bar-main-container red">
+    <div class="wrap">
+      <div class="bar-percentage" data-percentage="60">Alpha - 50 Pts</div>
+      <div class="bar-container">
+          <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:50%"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
